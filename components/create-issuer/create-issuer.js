e91e5d7c054db5be18a576f91a89d4bb022275f3
Dzvonkevich <dzvonkevich@gmail.com>
2017-11-27 23:34:41 +0200

    :wrench: fix property name

--- components/create-issuer/create-issuer.js
+++ components/create-issuer/create-issuer.js
@@ -13,28 +13,28 @@ class CreateIssuer extends Component {
 		// fields of the form
 		this._fields  = {
 			name: `
-	            <div class="form-group">
-	                <input name="name" type="text" class="form-control" placeholder="Issuer name" required="">
-	            </div>
-	        `,
+        <div class="form-group">
+            <input name="name" type="text" class="form-control" placeholder="Issuer name" required="">
+        </div>
+      `,
 			currency: `
-	            <div class="form-group">
-	                <select class="form-control m-b" name="currency">
-	                    <option value="usd">USD</option>
-	                    <option value="eur">EUR</option>
-	                </select>
-	            </div>
+        <div class="form-group">
+          <select class="form-control m-b" name="currency">
+            <option value="usd">USD</option>
+            <option value="eur">EUR</option>
+          </select>
+        </div>
 			`,
 		};
 
 		// will be rendered
 		this._html = `
-	        <h2>Create Issuer</h2>
-	        <div>
-				${this._fields.name}
-				${this._fields.currency}
-			</div>
-            <button type="submit" class="btn btn-primary block full-width m-b">Submit</button>
+    <h2>Create Issuer</h2>
+    <div>
+			${this._fields.name}
+			${this._fields.currency}
+		</div>
+    <button type="submit" class="btn btn-primary block full-width m-b">Submit</button>
 		`
 
 		// render component
